4bf92326e5427c19311ea4be754e7d790f880bf4
jheisson  rojas <jfrojas592@misena.edu.co>
2024-09-09 18:39:33 -0500

    pipes

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,7 +1,9 @@
 
 
 
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { log } from 'node:console';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -10,12 +12,34 @@ import { Component } from '@angular/core';
   <input type="text"
   class="form-control"
   placeholder="Buscar gifs...."
+  (keyup.enter)="searchTag (  )"
+  #txtTagInput
   >
 
   `
 })
 
 export class SearchBoxComponent {
-  constructor() { }
+
+@ViewChild('txtTagInput')
+public tagInput!: ElementRef<HTMLInputElement>;
+
+
+  constructor( private gifsService: GifsService) { }
+
+ // searchTag ( newTag:string ) {
+  searchTag ( ) {
+
+    const newTag = this.tagInput.nativeElement.value
+
+    this.gifsService.searchTag ( newTag );
+
+    this.tagInput.nativeElement.value = '';
+
+
+
+
+  }
+
 
 }
